2ac51bba72219d3465d91510cbb37d6d48120936
SKas7 <63898012+SKas7@users.noreply.github.com>
2024-05-02 20:00:26 -0500

    Update

---
+++ 19.3-activities/solved/js/index.js
@@ -0,0 +1,28 @@
+//Third Activity
+function fade() {
+$('.preloader').fadeOut("slow");
+}
+setTimeout(fade, 3000);
+
+
+// Second Activity 
+$(".searchButton").on("click", function(){
+  $(".searchButton").toggleClass("active");
+  if ($(".searchButton").hasClass("active")) {
+    $(".searchBar").css("height", "100vh");
+    $("#searchForm").css("opacity","1");
+    $(".searchIcon").removeClass("fa-search").addClass("fa-times");
+  }
+  else {
+    $(".searchBar").css('height', "0vh");
+    $("#searchForm").css("opacity","0");
+    $(".searchIcon").removeClass("fa-times").addClass("fa-search");
+  }
+});
+
+// First Activity 
+$(".toggle").click(function(){
+  $(this).find(".toggleContainer").toggleClass("active");
+  $(this).siblings(".toggle").find(".toggleContainer").removeClass("active");
+});
+ 
